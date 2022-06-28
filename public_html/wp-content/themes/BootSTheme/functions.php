984a74d98f6a12f72945def2f523c99f515e156d
Andrey Efremov <justfly915@gmail.com>
2022-06-28 15:18:50 +0300

    установка Carbon Fields

--- public_html/wp-content/themes/BootSTheme/functions.php
+++ public_html/wp-content/themes/BootSTheme/functions.php
@@ -1,4 +1,22 @@
 <?php
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' ); // Для версии 2.0 и выше
+function crb_attach_theme_options() {
+	Container::make( 'theme_options', __( 'Theme Options', 'crb' ) )
+		->add_fields( array(
+			Field::make( 'text', 'crb_text', 'Text Field' ),
+		) );
+}
+
+add_action( 'after_setup_theme', 'crb_load' );
+function crb_load() {
+	require_once( ABSPATH . '/vendor/autoload.php' );
+	\Carbon_Fields\Carbon_Fields::boot();
+}
+
 /**
  * Подключение стилей и скриптов
  */
